fc7d921b04ef7258ede16a85da0e35cf6ba444df
Felix Palmer <felixpalmer@gmail.com>
2013-12-17 13:36:30 +0100

    Animate away unused buttons rather than hiding

--- Cherrola/Panel.m
+++ Cherrola/Panel.m
@@ -1,4 +1,6 @@
 #import "Panel.h"
+#define FADE_DURATION 0.5
+#define SLIDE_DISTANCE 10
 
 @interface Panel() {
   NSDateFormatter *_formatter;
@@ -26,30 +28,64 @@
 - (void)configureForState:(enum TIMERSTATE)state
 {
   NSLog(@"state %u", state);
+  [NSAnimationContext beginGrouping];
+  [[NSAnimationContext currentContext] setDuration:FADE_DURATION];
   switch (state) {
     case OFF:
-      [[[self message] cell] setTitle:@"Start Pomodoro"];
-      [[self startButton] setEnabled:YES];
-      [[self cancelButton] setEnabled:NO];
+      if (![[self message] isHidden]) {
+        NSRect frame = [[self message] frame];
+        frame.origin.y -= SLIDE_DISTANCE;
+        [[[self message] animator] setFrame:frame];
+      }
+      [[[self message] animator] setHidden:YES];
+      [[[self startButton] animator] setEnabled:YES];
+      [[[self startButton] animator] setHidden:NO];
+      [[[self cancelButton] animator] setEnabled:NO];
+      [[[self cancelButton] animator] setHidden:YES];
       break;
     case POMODORO:
-      [[[self message] cell] setTitle:@"Pomodoro Running"];
-      [[self startButton] setEnabled:NO];
-      [[self cancelButton] setEnabled:YES];
+      [[[self message] cell] setTitle:@"Time remaining"];
+      if ([[self message] isHidden]) {
+        NSRect frame = [[self message] frame];
+        frame.origin.y += SLIDE_DISTANCE;
+        [[[self message] animator] setFrame:frame];
+      }
+      [[[self message] animator] setHidden:NO];
+      [[[self startButton] animator] setEnabled:NO];
+      [[[self startButton] animator] setHidden:YES];
+      [[[self cancelButton] animator] setEnabled:YES];
+      [[[self cancelButton] animator] setHidden:NO];
       break;
     case POMODORO_ENDED:
-      [[[self message] cell] setTitle:@"Pomodoro Ended"];
-      [[self startButton] setEnabled:NO];
-      [[self cancelButton] setEnabled:NO];
+      [[[self message] cell] setTitle:@"Ended"];
+      if ([[self message] isHidden]) {
+        NSRect frame = [[self message] frame];
+        frame.origin.y += SLIDE_DISTANCE;
+        [[[self message] animator] setFrame:frame];
+      }
+      [[[self message] animator] setHidden:NO];
+      [[[self startButton] animator] setEnabled:NO];
+      [[[self startButton] animator] setHidden:YES];
+      [[[self cancelButton] animator] setEnabled:NO];
+      [[[self cancelButton] animator] setHidden:YES];
       break;
     case REST:
       [[[self message] cell] setTitle:@"Take a break"];
-      [[self startButton] setEnabled:NO];
-      [[self cancelButton] setEnabled:NO];
+      if ([[self message] isHidden]) {
+        NSRect frame = [[self message] frame];
+        frame.origin.y += SLIDE_DISTANCE;
+        [[[self message] animator] setFrame:frame];
+      }
+      [[[self message] animator] setHidden:NO];
+      [[[self startButton] animator] setEnabled:NO];
+      [[[self startButton] animator] setHidden:YES];
+      [[[self cancelButton] animator] setEnabled:NO];
+      [[[self cancelButton] animator] setHidden:YES];
       break;
     default:
       break;
   }
+  [NSAnimationContext endGrouping];
 }
 
 - (void)setTimeRemaining:(NSTimeInterval)remaining
